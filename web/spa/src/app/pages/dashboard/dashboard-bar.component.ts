454591c3417066df78f2fbdea9cdd458e2abaded
dmitry <dmitrijs.rahmanins@gmail.com>
2021-05-13 13:52:31 +0300

    dashboard d3 bar added

---
+++ web/spa/src/app/pages/dashboard/dashboard-bar.component.ts
@@ -0,0 +1,53 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngx-dashboard-bar',
+  template: `
+    <ngx-charts-bar-vertical-stacked
+      [results]="results"
+      [scheme]="colorScheme"
+      [xAxis]="showXAxis"
+      [yAxis]="showYAxis"
+      [legend]="showLegend"
+      [xAxisLabel]="xAxisLabel"
+      [showXAxisLabel]="showXAxis"
+      [yAxisLabel]="yAxisLabel"
+      [noBarWhenZero]="noBarWhenZero"
+    >
+    </ngx-charts-bar-vertical-stacked>
+  `,
+})
+export class DashboardBarComponent {
+  results = [
+    {
+      name: 'p2',
+      series: [
+        {name: 'won', value: 6},
+        {name: 'lost', value: 3},
+        {name: 'draw', value: 1},
+      ]
+    },
+    {
+      name: 'p1',
+      series: [
+        {name: 'won', value: 3},
+        {name: 'lost', value: 6},
+        {name: 'draw', value: 1},
+      ]
+    },
+    {name: 'p3', series: []},
+  ];
+
+  showLegend = true;
+  noBarWhenZero: false;
+
+  showXAxis = true;
+  xAxisLabel = 'Players';
+
+  showYAxis = true;
+  yAxisLabel = 'Win Rate';
+  colorScheme = {
+    domain: ['#00d68f', '#ff3d71', '#fa0']
+  };
+}
+
--- web/spa/src/app/pages/dashboard/dashboard.module.ts
+++ web/spa/src/app/pages/dashboard/dashboard.module.ts
@@ -3,16 +3,20 @@ import { NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
-import {Ng2SmartTableModule} from "ng2-smart-table";
+import {Ng2SmartTableModule} from 'ng2-smart-table';
+import {DashboardBarComponent} from './dashboard-bar.component';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
 
 @NgModule({
   imports: [
     NbCardModule,
     ThemeModule,
     Ng2SmartTableModule,
+    NgxChartsModule,
   ],
   declarations: [
     DashboardComponent,
+    DashboardBarComponent,
   ],
 })
 export class DashboardModule { }
