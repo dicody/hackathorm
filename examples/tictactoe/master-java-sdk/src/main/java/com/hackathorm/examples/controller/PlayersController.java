6cd2f88073775229c4a6a48c09b40566ca8160e4
dicody <dmitrijs.rahmanins@gmail.com>
2021-08-14 23:21:51 +0300

    dockerfile added for java components + minor changes on master example (add player method)

--- examples/tictactoe/master-java-sdk/src/main/java/com/hackathorm/examples/controller/PlayersController.java
+++ examples/tictactoe/master-java-sdk/src/main/java/com/hackathorm/examples/controller/PlayersController.java
@@ -1,30 +1,43 @@
 package com.hackathorm.examples.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hackathorm.examples.domain.PlayerDto;
+import com.hackathorm.examples.domain.PlayerRequest;
 import com.hackathorm.examples.service.GameService;
 import io.dapr.Topic;
 import io.dapr.client.domain.CloudEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class PlayersController {
 
     private final GameService gameService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Topic(name = "players", pubsubName = "pubsub")
     @PostMapping(path = "/players")
-    public Mono<Void> handleMessage(@RequestBody(required = false) CloudEvent cloudEvent) {
+    public Mono<Void> handleMessage(@RequestBody CloudEvent cloudEvent) {
         return Mono.fromRunnable(() -> {
+            PlayerRequest request;
+            if (cloudEvent.getSource() != null) {
+                log.info("got player request data from event: {}", cloudEvent.getData());
+                request = objectMapper.convertValue(cloudEvent.getData(), PlayerRequest.class);
+            } else {
+                request = (PlayerRequest) cloudEvent.getData();
+            }
+            log.info("got player request: {}", request);
+
             // todo define model for player request!
             PlayerDto player = new PlayerDto();
-//            player.setName(request.getName());
-//            player.setName(((PlayerRequest) cloudEvent.getData()).getName());
-            player.setServiceAppId(cloudEvent.getSource());
+            player.setName(request.getName());
+            player.setServiceAppId(request.getServiceId());
             gameService.addPlayerToQueue(player);
         });
     }
--- examples/tictactoe/master-java-sdk/src/main/java/com/hackathorm/examples/domain/PlayerRequest.java
+++ examples/tictactoe/master-java-sdk/src/main/java/com/hackathorm/examples/domain/PlayerRequest.java
@@ -5,4 +5,5 @@ import lombok.Data;
 @Data
 public class PlayerRequest {
     private String name;
+    private String serviceId;
 }
